a83dbe398b7a2e80a6a4a647262de1a94c631013
mv <michvett.mv@gmail.com>
2014-11-19 14:17:00 +0100

    Remove Extra Settings Category for now
    This reverts commit 66337dd700852abeb8189e12dfce3999905bbe60.
    
    Conflicts:
    	res/values/mv_strings.xml
    	res/xml/extra_settings.xml

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -239,8 +239,7 @@ public class SettingsActivity extends Activity
             R.id.print_settings,
             R.id.nfc_payment_settings,
             R.id.home_settings,
-            R.id.dashboard,
-            R.id.extra_settings
+            R.id.dashboard
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
